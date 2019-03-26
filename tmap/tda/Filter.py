d4c8b64e2131dc1090005072dde27a5b648060ef
liaoth <l0404th@gmail.com>
2019-03-26 15:37:02 +0800

    solve dependency problem due to scikit-bio

--- tmap/tda/Filter.py
+++ tmap/tda/Filter.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-from sklearn import decomposition, manifold
-from tmap.tda.metric import Metric
 import umap
-from scipy.spatial.distance import pdist,squareform
-from skbio.stats import ordination
+from scipy.spatial.distance import pdist, squareform
+from sklearn import decomposition, manifold
+
+from tmap.tda.metric import Metric
 
 
 _METRIC_BUILT_IN = ["braycurtis", "canberra", "chebyshev", "cityblock", "correlation", "cosine", "dice", "euclidean",
@@ -12,11 +12,11 @@ _METRIC_BUILT_IN = ["braycurtis", "canberra", "chebyshev", "cityblock", "correla
                     "russellrao", "seuclidean", "sokalmichener", "sokalsneath", "sqeuclidean", "yule"]
 
 
-
 class Filters(object):
     """
     filters for TDA Mapper, to project data points onto a low dimensional space
     """
+
     def __init__(self, components=[0, 1], metric=None):
         if len(components) == 0:
             raise Exception("At least one filter is needed: %s" % str(components))
@@ -104,7 +104,7 @@ class GaussianDensity(Filters):
         """
         data = self._check_data(data)
         dist_matrix = self.metric.fit_transform(data)
-        return np.sum(np.exp(-(dist_matrix**2 / (2 * self.h))), axis=1).reshape(data.shape[0], 1)
+        return np.sum(np.exp(-(dist_matrix ** 2 / (2 * self.h))), axis=1).reshape(data.shape[0], 1)
 
 
 class PCA(Filters):
@@ -114,10 +114,10 @@ class PCA(Filters):
         components: The axis of projection. If you use components 0 and 1, this is [0, 1].
     """
 
-    def __init__(self, components=[0, 1],random_state=None, **kwds):
+    def __init__(self, components=[0, 1], random_state=None, **kwds):
         # PCA only accept raw data and calculate euclidean distance "internally"
         super(PCA, self).__init__(components=components, metric=None)
-        self.pca = decomposition.PCA(n_components=max(self.components) + 1,random_state=random_state, **kwds)
+        self.pca = decomposition.PCA(n_components=max(self.components) + 1, random_state=random_state, **kwds)
 
     def fit_transform(self, data):
         """
@@ -167,6 +167,7 @@ class MDS(Filters):
     Params:
         components: The axis of projection. If you use components 0 and 1, this is [0, 1].
     """
+
     def __init__(self, components=[0, 1], metric=Metric(metric="euclidean"), **kwds):
         super(MDS, self).__init__(components=components, metric=metric)
 
@@ -180,35 +181,43 @@ class MDS(Filters):
     def fit_transform(self, data):
         data = self._check_data(data)
         if self.metric.name != "euclidean" and self.metric.name != "precomputed":
-            data = squareform(pdist(data,metric=self.metric.name))
+            data = squareform(pdist(data, metric=self.metric.name))
             data = self.metric.fit_transform(data)
         else:
             data = self.metric.fit_transform(data)
         projected_data = self.mds.fit_transform(data)
         return projected_data[:, self.components]
 
+
 class PCOA(Filters):
+
     """
     PCoA filters.
     Implements with skbio.stats.ordination.pcoa
     Params:
         components: The axis of projection. If you use components 0 and 1, this is [0, 1].
     """
+
     def __init__(self, metric=Metric(metric="euclidean"), **kwds):
         super(PCOA, self).__init__()
         self.metric = metric
 
     def fit_transform(self, data):
+        # try:
+        from skbio.stats.ordination import pcoa
+        # except:
+        #     sys.exit("PCOA is using the pcoa implemened at scikit-bio.")
         data = self._check_data(data)
         if self.metric.name != "precomputed":
-            data = squareform(pdist(data,metric=self.metric.name))
+            data = squareform(pdist(data, metric=self.metric.name))
             data = self.metric.fit_transform(data)
         else:
             data = self.metric.fit_transform(data)
 
-        projected_data = ordination.pcoa(data)
+        projected_data = pcoa(data)
         self.pcoa = projected_data
         return self.pcoa.samples.values[:, self.components]
+
 
 class UMAP(Filters):
     """
@@ -216,15 +225,16 @@ class UMAP(Filters):
     Params:
         components: The axis of projection. If you use components 0 and 1, this is [0, 1].
     """
+
     def __init__(self, components=[0, 1], metric=Metric(metric="euclidean"), **kwds):
         super(UMAP, self).__init__(components=components, metric=metric)
 
         if self.metric.name in _METRIC_BUILT_IN:
             self.umap = umap.UMAP(n_components=max(self.components) + 1,
-                                  metric=self.metric.name,  **kwds)
+                                  metric=self.metric.name, **kwds)
         else:
             self.umap = umap.UMAP(n_components=max(self.components) + 1,
-                                  metric="precomputed",  **kwds)
+                                  metric="precomputed", **kwds)
 
     def fit_transform(self, data):
         data = self._check_data(data)
@@ -233,11 +243,12 @@ class UMAP(Filters):
         projected_data = self.umap.fit_transform(data)
         return projected_data[:, self.components]
 
-_filter_dict = {'L1Centrality':L1Centrality,
- 'LinfCentrality':LinfCentrality,
- 'GaussianDensity':GaussianDensity,
- 'PCA':PCA,
- 'TSNE':TSNE,
- 'MDS':MDS,
- 'PCOA':PCOA,
- 'UMAP':UMAP}
+
+_filter_dict = {'L1Centrality': L1Centrality,
+                'LinfCentrality': LinfCentrality,
+                'GaussianDensity': GaussianDensity,
+                'PCA': PCA,
+                'TSNE': TSNE,
+                'MDS': MDS,
+                'PCOA': PCOA,
+                'UMAP': UMAP}
