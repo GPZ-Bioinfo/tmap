12113bb8fca42ba24ead71672eb7e2ad2fa85d3a
liaoth <l0404th@gmail.com>
2019-03-26 10:35:31 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+import os
+import shutil
 import sys
+from distutils.command.clean import clean as Clean
 
 from setuptools import setup, find_packages
 
@@ -6,6 +9,36 @@ if sys.version_info < (3, 5):
     sys.exit("tmap can only be used with Python 3. You are currently "
              "running Python %d." % sys.version_info.major)
 
+
+class CleanCommand(Clean):
+    description = "Remove build artifacts from the source tree"
+
+    def run(self):
+        Clean.run(self)
+        # Remove c files if we are not within a sdist package
+        cwd = os.path.abspath(os.path.dirname(__file__))
+        remove_c_files = not os.path.exists(os.path.join(cwd, 'PKG-INFO'))
+        if remove_c_files:
+            print('Will remove generated .c files')
+        if os.path.exists('build'):
+            shutil.rmtree('build')
+        for dirpath, dirnames, filenames in os.walk('sklearn'):
+            for filename in filenames:
+                if any(filename.endswith(suffix) for suffix in
+                       (".so", ".pyd", ".dll", ".pyc")):
+                    os.unlink(os.path.join(dirpath, filename))
+                    continue
+                extension = os.path.splitext(filename)[1]
+                if remove_c_files and extension in ['.c', '.cpp']:
+                    pyx_file = str.replace(filename, extension, '.pyx')
+                    if os.path.exists(os.path.join(dirpath, pyx_file)):
+                        os.unlink(os.path.join(dirpath, filename))
+            for dirname in dirnames:
+                if dirname == '__pycache__':
+                    shutil.rmtree(os.path.join(dirpath, dirname))
+
+
+cmdclass = {'clean': CleanCommand}
 
 setup(name='tmap',
       version='1.1.3',
@@ -24,9 +57,7 @@ setup(name='tmap',
                'tmap/api/SAFE_analysis.py',
                'tmap/api/SAFE_visualization.py',
                'tmap/api/quick_vis.py'],
-      extras_require={'alldeps': ('numpy')},
-      install_requires=['scikit-bio',
-                        'statsmodels>=0.9.0',
+      install_requires=['statsmodels>=0.9.0',
                         'tqdm',
                         'scikit-learn>=0.19.1',
                         'matplotlib>=2.2.2',
@@ -37,7 +68,9 @@ setup(name='tmap',
                         'umap-learn',
                         'rpy2',
                         'plotly',
+                        'numpy'
                         ],
+      cmdclass=cmdclass,
       classifiers=[
           "Programming Language :: Python :: 3",
           "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
